fd87666bddda337727036723078d0930e70e6612
Vashistha_ Pratikshit (pvashistha@deloitte.com) <pvashistha@your.hostname.com>
2023-07-14 10:04:33 +0530

    service file

---
+++ service/Service.js
@@ -0,0 +1,8 @@
+import { matches } from "../data/matches.js";
+import { teams } from "../data/teams.js";
+export const getAllMatchesData = () => {
+  return matches;
+};
+export const getAllTeamsData = () => {
+  return teams;
+};
