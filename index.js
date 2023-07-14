553a2725d1b44363e04886d69f14af133910532c
Vashistha_ Pratikshit (pvashistha@deloitte.com) <pvashistha@your.hostname.com>
2023-07-14 10:01:59 +0530

    initialization of project

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import router from "./routes/apiRoutes.js";
+const app = express();
+const PORT = 8000;
+app.use(express.json());
+app.use("/api", router);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server started on port`);
+});
