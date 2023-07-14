41753cf9608281b31cc8b6fcc131bc19ceffa69c
Vashistha_ Pratikshit (pvashistha@deloitte.com) <pvashistha@your.hostname.com>
2023-07-14 10:05:08 +0530

    dummy data file

---
+++ data/matches.js
@@ -0,0 +1,49 @@
+export const matches = [
+  {
+    id: "1",
+
+    team1: "Team A",
+
+    team2: "Team B",
+
+    date: new Date("2023-01-03"),
+
+    score: {
+      team1: 250,
+
+      team2: 200,
+    },
+  },
+
+  {
+    id: "2",
+
+    team1: "Team C",
+
+    team2: "Team D",
+
+    date: new Date("2025-12-01"),
+
+    score: {
+      team1: 200,
+
+      team2: 250,
+    },
+  },
+
+  {
+    id: "3",
+
+    team1: "Team A",
+
+    team2: "Team C",
+
+    date: new Date("2020-01-09"),
+
+    score: {
+      team1: 150,
+
+      team2: 152,
+    },
+  },
+];
---
+++ data/teams.js
@@ -0,0 +1,153 @@
+export const teams = [
+  {
+    id: "1",
+
+    name: "Team A",
+
+    players: [
+      {
+        id: "1",
+
+        name: "Player A",
+
+        score: 100,
+
+        wickets: 3,
+      },
+
+      {
+        id: "2",
+
+        name: "Player B",
+
+        score: 10,
+
+        wickets: 8,
+      },
+
+      {
+        id: "3",
+
+        name: "Player C",
+
+        score: 160,
+
+        wickets: 0,
+      },
+    ],
+  },
+
+  {
+    id: "2",
+
+    name: "Team B",
+
+    players: [
+      {
+        id: "1",
+
+        name: "Player D",
+
+        score: 120,
+
+        wickets: 5,
+      },
+
+      {
+        id: "2",
+
+        name: "Player E",
+
+        score: 0,
+
+        wickets: 2,
+      },
+
+      {
+        id: "3",
+
+        name: "Player F",
+
+        score: 0,
+
+        wickets: 0,
+      },
+    ],
+  },
+
+  {
+    id: "3",
+
+    name: "Team C",
+
+    players: [
+      {
+        id: "1",
+
+        name: "Player G",
+
+        score: 67,
+
+        wickets: 2,
+      },
+
+      {
+        id: "2",
+
+        name: "Player H",
+
+        score: 90,
+
+        wickets: 4,
+      },
+
+      {
+        id: "3",
+
+        name: "Player I",
+
+        score: 60,
+
+        wickets: 0,
+      },
+    ],
+  },
+
+  {
+    id: "4",
+
+    name: "Team D",
+
+    players: [
+      {
+        id: "1",
+
+        name: "Player J",
+
+        score: 40,
+
+        wickets: 1,
+      },
+
+      {
+        id: "2",
+
+        name: "Player K",
+
+        score: 50,
+
+        wickets: 5,
+      },
+
+      {
+        id: "3",
+
+        name: "Player L",
+
+        score: 50,
+
+        wickets: 0,
+      },
+    ],
+  },
+];
